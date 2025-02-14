b7601de768f2959601d7e3ae1354dfe94f167642
arjun <arjun.m010011@gmail.com>
2025-02-14 12:36:28 +0530

    changed the icon and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,15 @@
 import "./globals.css";
+export const metadata = {
+  title: "notes app",
+  icons: "/favicon.ico",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        {/* Add this manually */}
+      </head>
       <body>{children}</body>
     </html>
   );
